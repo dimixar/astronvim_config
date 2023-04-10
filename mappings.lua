89a5588d0309799fc4a3bea66da037969cc1f56c
Dumitru Moloșnic <dimasalmaz@gmail.com>
2023-04-10 20:04:02 +0300

    Setup mappings for diffview toggling.

--- mappings.lua
+++ mappings.lua
@@ -2,14 +2,19 @@ return {
     n = {
         ["<leader>c"] = {
             function()
-                local bufs = vim.fn.getbufinfo {buflisted = true}
+                local bufs = vim.fn.getbufinfo { buflisted = true }
                 require("astronvim.utils.buffer").close(0)
                 if require("astronvim.utils").is_available "alpha-nvim" and
-                    not bufs[2] then require("alpha").start(true) end
+                    not bufs[2] then
+                    require("alpha").start(true)
+                end
             end,
             desc = "Close buffer"
         },
-        ["<leader>fp"] = {"<cmd>Telescope projects<cr>", desc = "Find project"},
-        ["<leader>gn"] = {"<cmd>Neogit<cr>", desc = "Open Neogit"}
+        ["<leader>fp"] = { "<cmd>Telescope projects<cr>", desc = "Find project" },
+        ["<leader>gn"] = { "<cmd>Neogit<cr>", desc = "Open Neogit" },
+        ["<leader>gdo"] = { "<cmd>DiffviewOpen<cr>", desc = "Open diffview" },
+        ["<leader>gdc"] = { "<cmd>DiffviewClose<cr>", desc = "Close diffview" },
+        ["<leader>gd"] = { "", desc = "Diffview opts" }
     }
 }
